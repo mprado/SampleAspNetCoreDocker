8bd5caf93cc2df0b9eef3fe49b726f6e64ec8519
Marcelo Prado <marcelo_prado@outlook.com>
2018-12-24 00:25:15 -0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SampleAspNetCoreDocker
 Sample ASP.NET Core 2.1 running in Docker container.
+
+
+docker build -t sample-aspnetcore-docker .
+
+docker run -d -p 5000:80 sample-aspnetcore-docker
---
+++ SampleAspNetCoreDocker.Api/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAspNetCoreDocker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Olá mamãe, tô no Docker! :)";
+        }
+    }
+}
